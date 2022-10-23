483833bfcb5f7fd4b10ebec9ad0a86a2f4e229d9
Your Name <you@example.com>
2022-10-23 18:14:27 -0500

    f

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,6 +2,6 @@ import { createStore, applyMiddleware , compose} from 'redux';
 import reducer from '../reducer/reducer';
 import thunk from 'redux-thunk';
 
-const store = createStore(reducer, compose(applyMiddleware(thunk), process.env.NODE_ENV !== 'production'?  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(): null));
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
 
 export default store;
